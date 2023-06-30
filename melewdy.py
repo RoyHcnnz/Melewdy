147c5f602b32048e20f6fe10b35d19906b4a5678
RoyHcnnz <rhuangcnnz@gmail.com>
2023-06-29 23:17:20 -0700

    get slash command working

--- melewdy.py
+++ melewdy.py
@@ -6,8 +6,17 @@ TOKEN = secret.BOT_TOKEN
 intents=discord.Intents.all()
 client = discord.Client(intents=intents)
 
+tree = discord.app_commands.CommandTree(client)
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    await tree.sync(guild=discord.Object(id=secret.TESTING_SERVER_ID))
+
+
+#Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+@tree.command(name = "search", description = "Search for songs", guild=discord.Object(id=secret.TESTING_SERVER_ID)) 
+async def searchSong(interaction, keyword: str):
+    await interaction.response.send_message("Searching for " + keyword + " ...")
 
 client.run(TOKEN)
